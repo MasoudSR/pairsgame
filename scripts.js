fa2040816aa1b11aca932d1c45aff715b6838e54
MasoudSRad <amir.sepehr72@gmail.com>
2022-10-05 17:49:27 +0330

    correct and wrong choice functions added

--- scripts.js
+++ scripts.js
@@ -42,22 +42,31 @@ let selected = "";
 function clickHandler(event) {
 	event.target.childNodes[0].style.visibility = "visible";
 	if (selected.length === 0) {
+		// first card chosen
 		selected = event.target;
 	} else {
+		// second card chosen
 		if (selected.innerText === event.target.innerText) {
-			// correctChoice()
-			event.target.style.pointerEvents = "none";
-			selected.style.pointerEvents = "none";
-			selected = "";
+			correctChoice(event);
 		} else {
-			const temp = selected;
-			setTimeout(() => {
-				event.target.childNodes[0].style.visibility = "hidden";
-				temp.childNodes[0].style.visibility = "hidden";
-			}, 1000);
-			selected = "";
+			wrongChoice(event);
 		}
 	}
+}
+
+function correctChoice(event) {
+	event.target.style.pointerEvents = "none";
+	selected.style.pointerEvents = "none";
+	selected = "";
+}
+
+function wrongChoice(event) {
+	const temp = selected;
+	setTimeout(() => {
+		event.target.childNodes[0].style.visibility = "hidden";
+		temp.childNodes[0].style.visibility = "hidden";
+	}, 1000);
+	selected = "";
 }
 
 startGame();
