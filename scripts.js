26a38a5e374bd56aa481ef5e7d94e00d9cd9a353
MasoudSRad <amir.sepehr72@gmail.com>
2022-10-05 17:32:36 +0330

    shuffle cards added

--- scripts.js
+++ scripts.js
@@ -1,59 +1,63 @@
 function startGame() {
-    cardGenerator()
-    document.querySelectorAll(".card").forEach((card) => {
-    card.addEventListener("click", clickHandler);
-    });
-    
+	cardGenerator();
+	document.querySelectorAll(".card").forEach((card) => {
+		card.addEventListener("click", clickHandler);
+	});
 }
 
 let selected = "";
 
 function clickHandler(event) {
-    event.target.childNodes[0].style.visibility="visible"
+	event.target.childNodes[0].style.visibility = "visible";
 	if (selected.length === 0) {
 		selected = event.target;
 	} else {
 		if (selected.innerText === event.target.innerText) {
-            event.target.style.pointerEvents="none"
-            selected.style.pointerEvents="none"
+			event.target.style.pointerEvents = "none";
+			selected.style.pointerEvents = "none";
 			selected = "";
 		} else {
-            const temp = selected
-            setTimeout(()=>{
-                event.target.childNodes[0].style.visibility="hidden"
-                temp.childNodes[0].style.visibility="hidden"
-            },1000)
-            selected = "";
+			const temp = selected;
+			setTimeout(() => {
+				event.target.childNodes[0].style.visibility = "hidden";
+				temp.childNodes[0].style.visibility = "hidden";
+			}, 1000);
+			selected = "";
 		}
 	}
 }
 
-function cardGenerator(){
-    const cardsPics = [1,2,3,4,5]
-    const cards = []
-    cardsPics.forEach((pic , index)=>{
-        const card = document.createElement("div")
-        card.classList.add("card")
-        card.id=index
-        card.innerHTML=`<p>${pic}</p>`
-        cards.push(card)
-    })
-    cardsPics.forEach((pic , index)=>{
-        const card = document.createElement("div")
-        card.classList.add("card")
-        card.id=index+5
-        card.innerHTML=`<p>${pic}</p>`
-        cards.push(card)
-    })
-    // shuffle(cards)
-    cards.forEach(card=>{
-        document.querySelector(".cards").appendChild(card)
-    })
+function cardGenerator() {
+	const cardsPics = [1, 2, 3, 4, 5];
+	const cards = [];
+	cardsPics.forEach((pic, index) => {
+		const card = document.createElement("div");
+		card.classList.add("card");
+		card.id = index;
+		card.innerHTML = `<p>${pic}</p>`;
+		cards.push(card);
+	});
+	cardsPics.forEach((pic, index) => {
+		const card = document.createElement("div");
+		card.classList.add("card");
+		card.id = index + 5;
+		card.innerHTML = `<p>${pic}</p>`;
+		cards.push(card);
+	});
+	shuffle(cards);
+	cards.forEach((card) => {
+		document.querySelector(".cards").appendChild(card);
+	});
 }
 
-// function shuffle(cards){
-//     let cardsIndex = cards.length
+function shuffle(cards) {
+	let currentIndex = cards.length;
+	let randomIndex;
+	while (currentIndex != 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+		[cards[currentIndex], cards[randomIndex]] = [cards[randomIndex], cards[currentIndex]];
+	}
+}
 
-// }
-
-startGame()
+startGame();
